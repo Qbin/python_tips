b5798da361ed857e7ddec163d12eaae891a78ddb
qinbinbin <qinbinbin@geotmt.com>
2018-06-14 17:46:31 +0800

    dynamically_get_result_of_expressions.py 增加注释

--- dynamically_get_result_of_expressions.py
+++ dynamically_get_result_of_expressions.py
@@ -1,4 +1,10 @@
 def dynamically_get_result_of_expressions(expressions, fields, *args):
+    """
+    :param expressions: python's expressions
+    :param fields: all variable of the expressions used
+    :param args: all value of variable what you want to assignment
+    :return: the result of the expressions when these variable assignment these value
+    """
     for i in range(len(fields)):
         locals()[fields[i]] = args[i]
     return eval(expressions)
